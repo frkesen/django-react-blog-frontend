ac00e23aa531f54e9304c42005e0ac4a2bbb71f4
Faruk <felixfranko66@gmail.com>
2021-01-28 03:33:21 +0300

    added postcard and fetchdata

---
+++ src/components/PostCard.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { red } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+// import { fetchData } from "../helper/FetchData";
+import PropTypes from "prop-types";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+export default function PostCard({
+    id,
+    title,
+    content,
+    imgSrc,
+    publishedDate,
+    author,
+    commentCount,
+    viewCount,
+    likeCount,
+}) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image={imgSrc} title={title} />
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            F
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={author}
+        subheader={publishedDate}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {content}
+        </Typography>
+      </CardContent>
+      <Grid container spacing={1} alignItems="center" >
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {`${likeCount} Likes`}
+        </Typography>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {`${viewCount} Views`}
+        </Typography>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {`${commentCount} Comments`}
+        </Typography>
+      </Grid> 
+    </Card>
+  );
+}
+
+PostCard.propTypes = {
+//   id: PropTypes.string.isRequired,
+  author: PropTypes.string,
+  title: PropTypes.string,
+  publishedDate: PropTypes.string,
+  imgSrc: PropTypes.string,
+  content: PropTypes.string,
+  likeCount: PropTypes.number,
+  viewCount: PropTypes.number,
+  commentCount: PropTypes.number,
+};
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+// import { fetchData } from "../helper/FetchData";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Container, CircularProgress } from "@material-ui/core";
+import { formatDateFunc } from "../helper/FormatDate";
+import PostCard from "../components/PostCard";
 
+const stylesFunc = makeStyles((theme) => ({
+  wrapper: {
+    marginTop: "10rem",
+    minHeight: "calc(100vh - 19.0625rem)",
+    textAlign: "center",
+  },
+  avatar: {
+    margin: "1rem auto",
+    backgroundColor: theme.palette.secondary.main,
+  },
+}));
 function Main() {
-    return (
-        <div>
-            Home Page
-        </div>
-    )
+//   const { id } = useParams();
+  const mainStyles = stylesFunc();
+
+  const [postData, setPostData] = useState();
+
+  // TODO: fill in catch finally
+  const fetchData = async () => {
+    const data = await axios.get("https://django-react-blog-prj.herokuapp.com/api/list")
+      .then((res) => setPostData(res?.data.results))
+    };
+    console.log(postData)
+
+    useEffect(() => {
+        fetchData();
+        }, []);
+
+    // useEffect(() => {
+    //     fetchData("https://django-react-blog-prj.herokuapp.com/api/list").then((res) => {
+    //     setPostData(res.data?.results);
+    //     });
+    // }, []);
+    // console.log(postData)
+
+  return (
+    <Container className={mainStyles.wrapper}>
+      {!postData ? (
+        <CircularProgress />
+      ) : (
+        <Grid container spacing={1}>
+          {postData?.map((post, key) => {
+            
+            return (
+              <Grid item sm={4} xs={6} key={key}>
+                {/* {post.title} */}
+                <PostCard
+                  id={post.id}
+                  title={post.title}
+                  content={post.text}
+                  imgSrc={post.image}
+                  publishedDate={formatDateFunc(post.published_date)}
+                  author={post.author}
+                  commentCount={post.get_comment_count}
+                  viewCount={post.get_view_count}
+                  likeCount={post.get_like_count}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      )}
+    </Container>
+  );
 }
 
 export default Main;
+            // "title": "learn django",
+            // "content": "learn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn djangolearn django",
+            // "image": "",
+            // "status": "p",
+            // "published_date": "2021-01-26T22:11:07.539180Z",
+            // "author": "felix",
+            // "slug": "learn-django-5eace78de0",
+            // "get_comment_count": 0,
+            // "get_view_count": 0,
+            // "get_like_count": 0
